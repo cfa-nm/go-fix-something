2e9383b5ee21692cdb0ae2f925e69b49b763a6ac
Ben Cates <ben.cates@gmail.com>
2013-07-13 15:00:25 -0600

    Moved flash messages to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def flash_messages
+    flash.map do |(type, value)|
+      content_tag :div, class: ['alert-box', type], data: {alert: true} do
+        h(value) + link_to(raw('&times;'), '#', class: 'close')
+      end
+    end.join.html_safe
+  end
+
   def modal(id, options = {}, &block)
     options[:class] = Array(options[:class]) + ['reveal-modal']
     content_tag :div, options.reverse_merge(id: id) do
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+  describe :flash_helper do
+    before { flash.now[:info] = "Test" }
+    subject { helper.flash_messages }
+
+    it { should have_css ".alert-box.info[data-alert]" }
+    it { should have_content "Test" }
+  end
+
   describe :modal do
     subject do
       helper.modal(:foo, class: 'bar') { "the content" }
